2d8831c8d1cab0da7d649f6227f8d9c459524f35
Nuttapong Phisitbutr <nuttapong@pen1.biz>
2021-12-27 11:16:33 +0700

    Create HomeScreenShortcut And AppWidget Projects

---
+++ HomeScreenShortcut/app/src/main/java/com/example/homescreenshortcut/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.homescreenshortcut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void createShortcut(View view){
+        Intent shortcutIntent = new Intent(this, MainActivity.class);
+        shortcutIntent.setAction(Intent.ACTION_MAIN);
+
+        Intent intent = new Intent();
+        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, getString(R.string.app_name));
+        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
+                Intent.ShortcutIconResource.fromContext(this, R.mipmap.ic_launcher));
+        intent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+        sendBroadcast(intent);
+    }
+}
