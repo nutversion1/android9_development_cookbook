78c42f190a3115c2820f1cea366c084a2b3c3010
Nuttapong Phisitbutr <nuttapong@pen1.biz>
2022-08-13 10:52:29 +0700

    Created GetDeviceOrientation project

---
+++ GetDeviceOrientation/app/src/main/java/com/example/getdeviceorientation/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.getdeviceorientation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void checkOrientation(View view) {
+        int orientation = getResources().getConfiguration().orientation;
+
+        switch (orientation){
+            case Configuration.ORIENTATION_LANDSCAPE:
+                Toast.makeText(
+                        MainActivity.this,
+                        "ORIENTATION_LANDSCAPE",
+                        Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            case Configuration.ORIENTATION_PORTRAIT:
+                Toast.makeText(
+                                MainActivity.this,
+                                "ORIENTATION_PORTRAIT",
+                                Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            case Configuration.ORIENTATION_UNDEFINED:
+                Toast.makeText(
+                                MainActivity.this,
+                                "ORIENTATION_UNDEFINED",
+                                Toast.LENGTH_SHORT)
+                        .show();
+                break;
+        }
+    }
+}
