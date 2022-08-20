59ad54f0996e05596dcec3bfffff599d64783da7
Nuttapong Phisitbutr <nuttapong@pen1.biz>
2022-08-20 19:51:02 +0700

    Created KotlinToast project

---
+++ KotlinToast/app/src/main/java/com/example/kotlintoast/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.kotlintoast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            Toast.makeText(this,
+                "show toast",
+                Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+}
